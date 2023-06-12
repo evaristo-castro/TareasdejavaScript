1b28bb2c59d2e0ed797ea595e9047a6c63c36666
EVARISTO <evariste96@hotmail.com>
2023-06-12 10:14:03 -0500

    Eliminar Carpeta

--- PROYECTO/SECCION-HOMBRE/javascripmenu.js
+++
@@ -1,8 +0,0 @@
-var toggleButton = document.getElementById('toggleButton');
-var menu = document.getElementById('menu');
-
-toggleButton.addEventListener('click', function(){
-
-    menu.classList.toggle('show');
-});
-
