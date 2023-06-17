38c81627b756f70f8ed81f6fe9a07376c8a2633f
EVARISTO <evariste96@hotmail.com>
2023-06-16 21:26:42 -0500

    Classes y objetos

---
+++ EJERCICIO CLASES/clasesPoo2/main.js
@@ -0,0 +1,12 @@
+//'use strict'
+
+//import { Persona } from "./classpersona.js";
+const Miobjeto = new Personaa('Evaris','Cast','eva@co',33);
+
+//llamar a los metodos
+console.log(Miobjeto.getDatos());
+console.log(Miobjeto.getClasificar());
+
+
+
+            
